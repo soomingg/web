3156a6938921d7a2d6bf0df5154063175c7b4d9e
soomingg <tnalsqkr0612@naver.com>
2023-09-23 16:05:09 +0900

    fourth

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', require('./routes/books'));        //require = import
+app.use('/locals', require('./routes/locals'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/locals.js
+++ routes/locals.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* 게시판 page. */
+/* 지역검색 home page. */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: '익스프레스', name:'홍길동' });      //posts뒤에 ejs 생략
+  res.render('index', { title: '지역검색',pageName:'locals/search.ejs' });
 });
 
 module.exports = router;
