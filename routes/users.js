936478f0bb1dd58869c9d02fdb08952ec778e07b
soomingg <tnalsqkr0612@naver.com>
2023-11-04 15:33:57 +0900

    week 10

--- routes/users.js
+++ routes/users.js
@@ -14,5 +14,10 @@ router.get('/login', function(req, res) {
 //회원가입 페이지
 router.get('/join', function(req, res) {
   res.render('index',{title:'회원가입',pageName:'users/join.ejs'});
-});            
+}); 
+
+//마이페이지
+router.get('/mypage', function(req, res){
+  res.render('index',{title:'마이페이지',pageName:'users/mypage.ejs'});
+});
 module.exports = router;
