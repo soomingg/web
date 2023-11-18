fb1c2ba2dad28b14bd4330f1ded43827589e26e0
soomingg <tnalsqkr0612@naver.com>
2023-11-18 15:44:34 +0900

    week 12

--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,9 @@ router.get('/', function(req, res){
 router.get('/insert', function(req, res){
     res.render('index',{title:'글쓰기', pageName:'posts/insert.ejs'});
 });
+router.get('/read', function(req, res, next){
+    const id=req.query.id;
+    res.render('index',{title:'게시글정보', pageName:'posts/read.ejs', id:id});
+});
 
 module.exports = router;
